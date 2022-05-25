47d6f4125e248c32e7579fe928ac8fb31102a0cf
myownsumm <myownsumm@gmail.com>
2022-05-25 17:10:51 +0300

    Resolve repos in router in a required way

--- src/app/modules/repos/resolvers/repos.resolver.ts
+++ src/app/modules/repos/resolvers/repos.resolver.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable, of, tap } from 'rxjs';
+import { forkJoin, map, Observable, switchMap, take, tap } from 'rxjs';
 import { AuthStateService } from '../../auth/services/auth-state.service';
 import { Repo } from '../repos.typings';
 import { ReposApiService } from '../services/repos-api.service';
@@ -17,9 +17,22 @@ export class ReposResolver implements Resolve<Repo[]> {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<Repo[]> {
-    return this.reposApiService.getRepos('some', 1).pipe(
-      tap(repos => {
-        this.reposStateService.setReposFetched(repos);
+    return this.authStateService.getAuthUser$().pipe(
+      take(1),
+      switchMap((authUser) => {
+        const pass = authUser.password;
+
+        return forkJoin([
+          this.reposApiService.getRepos(pass[0], 3000),
+          this.reposApiService.getRepos(pass[pass.length - 1], 5000),
+        ]).pipe(
+          map(([reposOne, reposTwo]) => {
+            const set = new Set([...reposOne, ...reposTwo]);
+
+            return Array.from(set);
+          }),
+          tap((items) => this.reposStateService.setReposFetched(items))
+        );
       })
     );
   }
