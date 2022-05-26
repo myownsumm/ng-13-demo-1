c4adb6adfa03effdf297f921cf72b69b76701593
myownsumm <myownsumm@gmail.com>
2022-05-26 12:13:45 +0300

    Clean files not needed

--- src/app/services/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-
-
-@Injectable()
-export class AppService {
-  constructor(private http: HttpClient) {}
-}
--- src/app/store/actions/app.actions.ts
+++
@@ -1,4 +0,0 @@
-import { Action } from '@ngrx/store';
-
-export enum ActionTypes {
-}
